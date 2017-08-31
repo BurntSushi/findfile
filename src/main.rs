2e88ec807baeb0ad8d83949fc1dbbdd9b69878a6
Anna Liao <aliao22@gmail.com>
2017-08-31 12:56:23 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file = File::open("poem.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("contents: {:?}", contents);
+}
